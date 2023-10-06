f6a38353bb6d7afaef91930b6ca27870800437ee
Yachen Wang <OriPoin@outlook.com>
2023-10-06 21:51:41 +0800

    data: use hdf5

--- script/fft1.m
+++ script/fft1.m
@@ -2,7 +2,15 @@ A=readtable('../data/N=512、4096点的一维信号.xlsx','ReadVariableName',fal
 C=readtable('../data/N=512、4096点的一维信号.xlsx','ReadVariableName',false,'Range','C1:C4096').(1);
 fftA = fft(A);
 fftC = fft(C);
-save('../data/data512.mat', "A")
-save('../data/data4096.mat', "C")
-save('../data/fft512.mat', "fftA")
-save('../data/fft4096.mat', "fftC")
+h5create("../data/fft1_512.h5", "/data512", size(A))
+h5write("../data/fft1_512.h5","/data512",A)
+h5create("../data/fft1_4096.h5", "/data4096", size(C))
+h5write("../data/fft1_4096.h5","/data4096",C)
+h5create("../data/fft1_512.h5", "/fft512_r", size(real(fftA)))
+h5write("../data/fft1_512.h5","/fft512_r",real(fftA))
+h5create("../data/fft1_512.h5", "/fft512_i", size(imag(fftA)))
+h5write("../data/fft1_512.h5","/fft512_i",imag(fftA))
+h5create("../data/fft1_4096.h5", "/fft4096_r", size(real(fftC)))
+h5write("../data/fft1_4096.h5","/fft4096_r",real(fftC))
+h5create("../data/fft1_4096.h5", "/fft4096_i", size(imag(fftC)))
+h5write("../data/fft1_4096.h5","/fft4096_i",imag(fftC))
--- script/fft2.m
+++ script/fft2.m
@@ -1,8 +1,16 @@
 data512_512 = imread("../data/512×512.jpg");
 fft512_512 = fft(im2double(data512_512));
-data4k_4k = imread("../data/4096×4096.jpg");
-fft4k_4k = fft(im2double(data4k_4k));
-save('../data/data512_512.mat', "data512_512")
-save('../data/data4k_4k.mat', "data4k_4k")
-save('../data/fft512_512.mat', "fft512_512")
-save('../data/fft4k_4k.mat', "fft4k_4k")
+data4096_4096 = imread("../data/4096×4096.jpg");
+fft4096_4096 = fft(im2double(data4096_4096));
+h5create("../data/fft2_512.h5", "/data512", size(data512_512))
+h5write("../data/fft2_512.h5","/data512",data512_512)
+h5create("../data/fft2_4096.h5", "/data4096", size(data4096_4096))
+h5write("../data/fft2_4096.h5","/data4096",data4096_4096)
+h5create("../data/fft2_512.h5", "/fft512_r", size(real(fft512_512)))
+h5write("../data/fft2_512.h5","/fft512_r",real(fft512_512))
+h5create("../data/fft2_512.h5", "/fft512_i", size(imag(fft512_512)))
+h5write("../data/fft2_512.h5","/fft512_i",imag(fft512_512))
+h5create("../data/fft2_4096.h5", "/fft4096_r", size(real(fft4096_4096)))
+h5write("../data/fft2_4096.h5","/fft4096_r",real(fft4096_4096))
+h5create("../data/fft2_4096.h5", "/fft4096_i", size(imag(fft4096_4096)))
+h5write("../data/fft2_4096.h5","/fft4096_i",imag(fft4096_4096))
